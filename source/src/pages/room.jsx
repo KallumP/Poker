b5d91c0a2892e15e513bafe6f55e18841cdc3965
Kallum Panditharatna <kallum179@gmail.com>
2022-10-11 16:22:56 +0100

    :construction: Player object created, holding information about this player

--- source/src/pages/room.jsx
+++ source/src/pages/room.jsx
@@ -1,11 +1,61 @@
 import Header from '../components/header';
 import { h1Style, ShuffleDeck, GenerateCardDeck } from '../utils';
-import Card from '../components/card';
 
-import React from 'react';
-import { Box, Stack, Item, Typography } from '@mui/material';
+import React, { useEffect, useState } from 'react';
+import { Box, Stack, Typography } from '@mui/material';
 
+const defaultPlayer = { money: 500, cards: [] }
 const Room = () => {
+
+	const [player, setPlayer] = useState([defaultPlayer]);
+	let shuffledDeck;
+
+	function setupRoom() {
+		generateCards();
+		setUpPlayer();
+	}
+
+	function generateCards() {
+
+		shuffledDeck = ShuffleDeck(GenerateCardDeck());
+		// console.log("Shuffled Cards: ", shuffledDeck);
+	}
+
+
+	function setUpPlayer() {
+		let tempPlayer = [...player];
+
+		let tempCardsList = [...tempPlayer[0].cards];
+		tempCardsList.push(getNextCard());
+		tempCardsList.push(getNextCard());
+		tempPlayer[0].cards = [...tempCardsList];
+
+		setPlayer(tempPlayer);
+
+		console.log("Player", player[0]);
+	}
+
+	useEffect(() => {
+		setupRoom();
+	}, []);
+
+
+	function getNextCard() {
+
+		let card = shuffledDeck[0];
+		removeNextCard();
+
+		let cardwKey = { key: card.value + card.suit, card: card };
+		console.log("Shuffled cards after shift: ", shuffledDeck);
+
+		return cardwKey;
+	}
+	function removeNextCard() {
+
+		shuffledDeck.splice(0, 1);
+
+	}
+
 	function renderPage() {
 		return (
 			<Box
@@ -19,21 +69,23 @@ const Room = () => {
 					Room
 				</Typography>
 
-				{renderCards()}
+				{renderSelf()}
 			</Box>
 		);
 	}
 
-	function renderCards() {
+	function renderSelf() {
 
-		let unshuffledDeck = GenerateCardDeck();
-		console.log("Unshuffled Cards: ", unshuffledDeck);
-		
-		let shuffledDeck = ShuffleDeck(unshuffledDeck);
-		console.log("Shuffled Cards: ", shuffledDeck);
-
+		let jsx = [];
+		if (player[0].cards.length > 0)
+			for (let i = 0; i < player[0].cards.length; i++) {
+				jsx.push(player[0].cards[i].card)
+			}
+		else
+			jsx.push("No cards");
 
 		return (
+
 			<Stack
 				spacing={5}
 				direction='row'
@@ -42,13 +94,12 @@ const Room = () => {
 					display: 'flex',
 					justifyContent: 'center',
 				}}>
-				{shuffledDeck[0]}
 
-				{shuffledDeck[1]}
+				{jsx}
 			</Stack>
-		);
-	}
 
+		)
+	}
 	return (
 		<Stack direction='column'>
 			<Header />
